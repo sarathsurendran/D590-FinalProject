dc6795d311f7d0aca0c02e95a364c3b0ac613e58
Surendran <sgsurend@iu.edu>
2022-12-10 14:51:14 -0600

    12/10

--- app.py
+++ app.py
@@ -5,25 +5,22 @@ import matplotlib.pyplot as plt
 import matplotlib
 import pickle
 import seaborn as sns
+from sklearn.model_selection import train_test_split, cross_val_score
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 matplotlib.pyplot.switch_backend('Agg') 
 # cfm = pickle.load(open('static/models/lr-cfm.pk1', 'rb'))
-df = pickle.load(open('static/models/test_df_dump.p', 'rb'))
+df = pickle.load(open('static/models/df_vehicles.p', 'rb'))
 # make_names=[*set(list((df["manufacturer"])))]
 # make_codes=[*set(list((df["make"])))]
 result=""
-
-price_range = (df['price'] >=24000) & (df['price'] <= 34000)
-df_price=df.loc[price_range]
 data=df.to_json(orient = 'records')
-df_price_json=df_price.to_json(orient = 'records')
 
  
 def loadcharts():
     #vehicles by region
-    df_reg=df["region"][:10].value_counts().reset_index()
+    df_reg=df["region"].value_counts()[:10].reset_index()
     df_reg.columns=["region","count"]
     plt.xticks(rotation=70)
     plt.title("Used vehicles available by region - top 10",size=15)
@@ -34,10 +31,11 @@ def loadcharts():
     #Vehicle count vs cylinders
     df_cylider=df["cylinders"].value_counts().reset_index()
     df_cylider.columns=["cylinders","count"]
-    plt.title("# of cyclinders",size=15)
+    plt.title("# of cyclinders",size=50)
     ax=sns.barplot(df_cylider['cylinders'],df_cylider['count'],color = 'b')
     fig=ax.get_figure()
-    fig.savefig("static/images/charts/clynd_count.png")
+    fig.set_size_inches(20, 10, forward=True)
+    fig.savefig("static/images/charts/clynd_count.png",dpi=100)
 
     #year of manufacturing
     plt.figure(figsize=(10,15))
@@ -63,7 +61,7 @@ loadcharts()
 
 @app.route('/')
 def homepage():
-    return render_template("home.html",data=data,data_price=df_price_json)
+    return render_template("home.html",data=data)
 
 @app.route('/predict_results', methods=['POST'])
 def process_qt_calculation():
@@ -71,7 +69,12 @@ def process_qt_calculation():
     data = request.get_json()
     print(type(data["make"]))
     
-   
+    cols=[]
+    if(data["make"] != "NA"):
+        cols.append("make")
+    if(data["model"] != "NA"):
+        cols.append("model_codes")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=50)
     df_make=df[(df['price']>1000) & (df['make'] == int(data['make']))]
     df_make_year=df_make[df_make['year'] == int(data['year'])]
     df_make_year_miles=df_make_year[df_make_year['odometer'] <= int(data['miles'])]
--- static/js/home.js
+++ static/js/home.js
@@ -8,7 +8,7 @@ $().ready(function(){
         var value = $(this).attr('value');
         console.log("Value selected :: " + value)
         $('#eda-chart').hide()
-        $('#eda-chart img').attr('width', '700').attr('height', '700');
+        $('#eda-chart img').attr('width', '900').attr('height', '750');
         if (value == 0) {
             $('#chartsdropdwnbtn span').text("Select Chart to display");
             $('#eda-chart').fadeOut(1000);
@@ -34,24 +34,35 @@ $().ready(function(){
     });
     
     
-    test=JSON.parse(data)
-    test_price=JSON.parse(data_price)
+    data_json=JSON.parse(data)
+
     var make_codes=new Set()
     var make_names=new Set()
-    var count=0;
-    $.each(test,function(index,item){
-        make_codes.add(test[index].make)
-        make_names.add(test[index].manufacturer)
-
+    var size_info=new Set()
+    var condition_info=new Set()
+    var veh_type=new Set()
+    var veh_transmission=new Set()
+    var make_model={}
+    $.each(data_json,function(index,item){
+        make_codes.add(data_json[index].make)
+        make_names.add(data_json[index].manufacturer)
+        size_info.add(data_json[index].size_codes+"~"+data_json[index].size)
+        condition_info.add(data_json[index].condition_codes+"~"+data_json[index].condition)
+        veh_type.add(data_json[index].type_codes+"~"+data_json[index].type)
+        veh_transmission.add(data_json[index].transmission_codes+"~"+data_json[index].transmission)
+        // creating a dictonary with make to model mapping
+        if(make_model.hasOwnProperty(data_json[index].make)){
+            var models=make_model[data_json[index].make]
+            models.add(data_json[index].model_codes+"~"+data_json[index].model)
+        }
+        else{
+            var model_codes=new Set()
+            model_codes.add(data_json[index].model_codes+"~"+data_json[index].model)
+            make_model[data_json[index].make]=model_codes
+        }
     })
 
-    make_codes_price=[]
-    $.each(test_price,function(index,item){
-        if(index <15){        
-            $('.predict-results tbody').append('<tr><td>'+(index+1)+'</td><td>'+item.model+'</td><td>'+item.type+'</td><td>'
-        +item.manufacturer+'</td><td>'+item.condition+'</td><td>'+item.price+'</td>')
-    }
-    })
+
 
     $('.pred-tab').click(function(){
         $('#predictdetails').hide();
@@ -62,24 +73,78 @@ $().ready(function(){
     $('#showresults').click(function(){
         $('.predict-results').show()
     })
-    console.log(make_codes)
+ 
     make_codes=Array.from(make_codes)
     make_names=Array.from(make_names)
+    size_info=Array.from(size_info)
+    condition_info=Array.from(condition_info)
+    veh_type=Array.from(veh_type)
+    veh_transmission=Array.from(veh_transmission)
     $.each(make_codes,function(i,item){    
-        $('.chosen-select').append('<option value="'+item+'">'+make_names[i]+'</option>')
+        $('.chosen-select-make').append('<option value="'+item+'">'+make_names[i]+'</option>')
     });
    
-    $(".chosen-select").chosen({no_results_text: "Oops, nothing found!"});
+  
+    $(".chosen-select-make .chosen-select-model").chosen({no_results_text: "Oops, nothing found!"});
+    $('.chosen-select-model ').chosen();
+    $('.chosen-select-size').chosen();
+    $('.chosen-select-condition').chosen();
+    $('.chosen-select-type').chosen();
+    $('.chosen-select-transmission').chosen();
 
+    //size
+    $.each(size_info,function(i,item){
+        model_struct=item.split("~")
+        $('.chosen-select-size').append('<option value="'+model_struct[0]+'">'+model_struct[1]+'</option>')
+    });
+    $('.chosen-select-size').trigger('chosen:updated')
+
+    //condition
+    $.each(condition_info,function(i,item){
+        model_struct=item.split("~")
+        $('.chosen-select-condition').append('<option value="'+model_struct[0]+'">'+model_struct[1]+'</option>')
+    });
+    $('.chosen-select-condition').trigger('chosen:updated')
+
+    //type
+    $.each(veh_type,function(i,item){
+        model_struct=item.split("~")
+        $('.chosen-select-type').append('<option value="'+model_struct[0]+'">'+model_struct[1]+'</option>')
+    });
+    $('.chosen-select-type').trigger('chosen:updated')
+
+    //transmission
+    $.each(veh_transmission,function(i,item){
+        model_struct=item.split("~")
+        $('.chosen-select-transmission').append('<option value="'+model_struct[0]+'">'+model_struct[1]+'</option>')
+    });
+    $('.chosen-select-transmission').trigger('chosen:updated')
+
+    //make
+    $('.chosen-select-make').chosen().change(function() {
+        $('.chosen-select-model').empty();
+        var models=make_model[$(this).val()];
+        models=Array.from(models)
+        $.each(models,function(i,item){    
+            model_struct=item.split("~")
+            
+            $('.chosen-select-model').append('<option value="'+model_struct[0]+'">'+model_struct[1]+'</option>')
+        });
+        $('.chosen-select-model').trigger('chosen:updated')
+        //$('#' + $(this).val()).show();
+    });
     $('button[id="predictbtn"').click(function(){
         $('form').hide();
         var server_data = {
-            "make": $('.chosen-select').chosen().val(),
-            "year": $('#vehicle-year').val(),
-            "miles": $('#vehicle-miles').val()
+            "make": "NA",
+            "year": 0,
+            "miles": 0,
+            "model":0
         };
 
-        
+        // make": $('.chosen-select-make').chosen().val(),
+        //     "year": $('#vehicle-year').val(),
+        //     "miles": $('#vehicle-miles').val()
 
         $.ajax({
             type: "POST",
@@ -93,12 +158,12 @@ $().ready(function(){
               $('#pred_price').text(result.predicted_price +" USD")
               $('#predictdetails').show();
               $('.predict-results tbody').empty() //remove all children
+
+              //parse through vehicles data and add to table
               veh_data=JSON.parse(result.data)
               $.each(veh_data,function(index,item){
-              
                 $('.predict-results tbody').append('<tr><td>'+(index+1)+'</td><td>'+item.model+'</td><td>'+item.type+'</td><td>'
-                +item.manufacturer+'</td><td>'+item.condition+'</td><td>'+item.price+'</td>')
-            
+                +item.manufacturer+'</td><td>'+item.condition+'</td><td>'+item.price+'</td>')          
             })
             } 
           });
